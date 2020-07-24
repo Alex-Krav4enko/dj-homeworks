53f1b9849f802205eb6196df1f67f71f4b1141ca
Alex Kravchenko <krav4enko@mail.ru>
2020-07-24 23:08:53 +0300

    ab-test init

--- request-handling/landing/app/views.py
+++ request-handling/landing/app/views.py
@@ -2,33 +2,43 @@ from collections import Counter
 
 from django.shortcuts import render_to_response
 
-# Для отладки механизма ab-тестирования используйте эти счетчики
-# в качестве хранилища количества показов и количества переходов.
-# но помните, что в реальных проектах так не стоит делать
-# так как при перезапуске приложения они обнулятся
 counter_show = Counter()
 counter_click = Counter()
 
+TEST = 'test'
+ORIGINAL = 'original'
+
 
 def index(request):
-    # Реализуйте логику подсчета количества переходов с лендига по GET параметру from-landing
+    from_landing = request.GET.get('from-landing')
+    if from_landing == ORIGINAL:
+        counter_click[ORIGINAL] += 1
+    elif from_landing == TEST:
+        counter_click[TEST] += 1
+
     return render_to_response('index.html')
 
 
 def landing(request):
-    # Реализуйте дополнительное отображение по шаблону app/landing_alternate.html
-    # в зависимости от GET параметра ab-test-arg
-    # который может принимать значения original и test
-    # Так же реализуйте логику подсчета количества показов
+    ab_test_arg = request.GET.get('ab-test-arg', 'original')
+
+    if ab_test_arg == TEST:
+        counter_show[TEST] += 1
+        return render_to_response('landing_alternate.html')
+
+    counter_show[ORIGINAL] += 1
     return render_to_response('landing.html')
 
 
 def stats(request):
-    # Реализуйте логику подсчета отношения количества переходов к количеству показов страницы
-    # Чтобы отличить с какой версии лендинга был переход
-    # проверяйте GET параметр marker который может принимать значения test и original
-    # Для вывода результат передайте в следующем формате:
+    try:
+        test_indicator = counter_click[TEST] / counter_show[TEST]
+        original_indicator = counter_click[ORIGINAL] / counter_show[ORIGINAL]
+    except ZeroDivisionError as eror:
+        test_indicator = 0
+        original_indicator = 0
+
     return render_to_response('stats.html', context={
-        'test_conversion': 0.5,
-        'original_conversion': 0.4,
+        'test_conversion': test_indicator,
+        'original_conversion': original_indicator,
     })
