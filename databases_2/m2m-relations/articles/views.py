f5735187e4060bbbfab5d15bb4a8b02e30b3daf8
Alex Kravchenko <krav4enko@mail.ru>
2020-08-30 21:22:04 +0300

    init second db task

--- databases_2/m2m-relations/articles/views.py
+++ databases_2/m2m-relations/articles/views.py
@@ -1,12 +1,15 @@
 from django.views.generic import ListView
 from django.shortcuts import render
 
-from articles.models import Article
+from .models import Article
+from .models import Relationship
 
 
 def articles_list(request):
     template = 'articles/news.html'
-    context = {}
+    # articles_with_deps = Relationship.objects.all()
+    articles = Article.objects.all()
+    context = {'object_list': articles}
 
     # используйте этот параметр для упорядочивания результатов
     # https://docs.djangoproject.com/en/2.2/ref/models/querysets/#django.db.models.query.QuerySet.order_by
